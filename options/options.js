5cd00534930b50237c7d97b1b7f6274ee1746216
Crowdbotics <engineering@crowdbotics.com>
2023-06-16 13:56:36 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "broken-bird-dev-92166",
+  url: "https://broken-bird-dev-92166.botics.co",
+  api: "https://broken-bird-dev-92166.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
